c4ede40320b71c6f1dae3909616527f3ffde321d
rain <rain.late@outlook.com>
2023-07-29 21:35:32 +0800

    fix:

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+
         </p>
-        <a
-          className="App-link"
+
+        <a className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
